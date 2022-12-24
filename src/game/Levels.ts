9ca3604b1fa8bcf07ac8d5f9d2db59d23997151b
Achim Völz <achim.voelz.1991@gmail.com>
2022-12-24 15:51:19 +0100

    added levels

---
+++ src/game/Levels.ts
@@ -0,0 +1,39 @@
+import { Position, Tile } from "./Game";
+
+export type CharacterDefinition = {
+    position: Position;
+    hp?: number | undefined;
+}
+
+export type LevelDefinition = {
+    level: Tile[][];
+    knight: CharacterDefinition;
+    dragon: CharacterDefinition;
+}
+
+export const levels: LevelDefinition[] = [
+    {
+        level: [
+            [Tile.ROAD, Tile.ROAD, Tile.ROAD],
+        ],
+        knight: { position: { row: 0, column: 0 } },
+        dragon: { position: { row: 0, column: 2 }, hp: 1 }
+    },
+    {
+        level: [
+            [Tile.HOLE, Tile.ROAD, Tile.ROAD],
+            [Tile.ROAD, Tile.ROAD, Tile.HOLE],
+        ],
+        knight: { position: { row: 1, column: 0 } },
+        dragon: { position: { row: 0, column: 2 }, hp: 1 }
+    },
+    {
+        level: [
+            [Tile.HOLE, Tile.HOLE, Tile.HOLE, Tile.HOLE, Tile.ROAD, Tile.ROAD, Tile.ROAD, Tile.ROAD],
+            [Tile.HOLE, Tile.HOLE, Tile.ROAD, Tile.ROAD, Tile.ROAD, Tile.HOLE, Tile.HOLE, Tile.HOLE],
+            [Tile.ROAD, Tile.ROAD, Tile.ROAD, Tile.HOLE, Tile.HOLE, Tile.HOLE, Tile.HOLE, Tile.HOLE]
+        ],
+        knight: { position: { row: 2, column: 0 } },
+        dragon: { position: { row: 0, column: 7 }, hp: 1 }
+    }
+]
