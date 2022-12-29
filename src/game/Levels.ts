297104e2bafcaa204cc50ee5f73901d317243a2e
Achim Völz <achim.voelz.1991@gmail.com>
2022-12-29 19:34:02 +0100

    level clean up

--- src/game/Levels.ts
+++ src/game/Levels.ts
@@ -27,8 +27,8 @@ export type LevelDefinition = {
     actions: number;
     exludedStatements?: StatementExlude[];
     extends?: string[],
-    solution?: string;
     help?: string,
+    solution?: string;
 }
 
 export const levels: LevelDefinition[] = [
@@ -40,27 +40,6 @@ export const levels: LevelDefinition[] = [
         dragon: { position: { row: 0, column: 2 }, hp: 1 },
         actions: 2,
         exludedStatements: [StatementExlude.IS_NEXT_TO, StatementExlude.IF, StatementExlude.WHILE, StatementExlude.NOT],
-        help: `
-        To attack the dragon you have to move your knight next to it and swing
-        your sword in it's direction like this: "knight.attack(<DIRECTION>);".
-        If you want to attack a dragon to the east, you can use "knight.attack(EAST);".
-        If you want to attack a dragon to the north, you can use "knight.attack(NORTH);".
-
-        To move the knight use "knight.move(<DIRECTION>);".
-        If you want to move your knight to the west, you can use "knight.move(WEST);".
-        If you want to move your knight to the south, you can use "knight.move(SOUTH);".
-
-        There are 4 directions available:
-        NORTH
-        EAST
-        WEST
-        SOUTH
-
-        You only have a certain amount of actions to slay the dragon. If you use more actions or
-        you don't slay it at all, you will be burned by the dragon.
-
-        Every 'move' and 'attack' statement will be counted as one action.
-        `
     },
     {
         level: [
@@ -91,36 +70,6 @@ export const levels: LevelDefinition[] = [
         knight: { position: { row: 1, column: 1 } },
         dragon: { position: [{ row: 1, column: 2 }, { row: 0, column: 1 }, { row: 2, column: 1 }, { row: 1, column: 0 }], hp: 1 },
         actions: 4,
-        help: `
-        Sometimes you won't know where the dragon is going to show up and it could be in multiple locations.
-
-        Your Knight can do more things!
-        
-        You can also check certain conditions of the level by using the 'isNextTo' function on
-        your characters. So if you want to check whether a knight is next to a certain TILE or character.
-
-        E.g. knight.isNextTo(EAST, WALL) checks whether the knight is next to a WALL on the EAST.
-        E.g. knight.isNextTo(SOUTH, dragon) checks whether the knigth is next to the dragon on the SOUTH.
-
-        If you use this with the 'if' statement you can do certain actions depending on certain states on
-        the level.
-        if(<condition>) {
-            <action>
-        } else {
-            <other action>
-        }
-
-        You don't have to use the 'else' part if you don't need it.
-        So for example:
-        if(knight.isNextTo(EAST, WALL)) {
-            knight.move(NORTH);
-        }
-
-        You can also negate the conditon by using the 'not' keyword like this:
-        if(not knight.isNextTo(EAST, WALL)) {
-            knight.move(NORTH);   
-        }
-        `,
         solution: `
         if(knight.isNextTo(WEST,dragon)) {
             knight.attack(WEST);
@@ -145,15 +94,6 @@ export const levels: LevelDefinition[] = [
         knight: { position: { row: 1, column: 0 } },
         dragon: { position: { row: 1, column: 7 }, hp: 1 },
         actions: 2,
-        help: `
-        Your Knight can do more things!
-        
-        There is a new statement for you to use 'while'.
-        With 'while' you can repeat an action as long as a certain condition is 'true'.
-        How to use: while(<condition>) {
-            <actions>
-        }
-        `
     },
     {
         level: [
@@ -236,13 +176,5 @@ export const levels: LevelDefinition[] = [
         mage: { position: { row: 0, column: 1 } },
         dragon: { position: { row: 2, column: 2 }, hp: 2 },
         actions: 4,
-        help: `
-        The Mage entered the game!
-        He can move around just like the Knight can, but he cannot attack.
-        Instead he can support the Knight by using his 'support' move like this:
-        'mage.support(<Direction>);'
-        e.g.
-        mage.support(SOUTH);
-        `
     },
 ]
